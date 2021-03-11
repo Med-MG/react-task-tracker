1f697a299eda53da786fe5ba4b32467d2df01402
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2021-03-11 17:38:40 +0100

    task reminder toggle

--- src/App.js
+++ src/App.js
@@ -21,11 +21,25 @@ const App = () => {
         day: 'Feb 7th at 2:30pm',
         reminder: true,
     }
-])
+  ])
+
+ //Delete Task
+ const DeleteTask = (id) => {
+  //  console.log('delete task ', id);
+  setTasks(tasks.filter((task) => task.id !== id));
+ }
+
+ //change reminder on toggle
+ const onToggle = (id) => {
+   setTasks(
+     tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+   )
+   
+ }
   return (
     <div className="container">
       <Header title='task tracker' />
-      <Tasks tasks={tasks} />
+       { tasks.length > 0 ? <Tasks tasks={tasks} onDelete= {DeleteTask} onToggle= {onToggle} /> : 'No tasks to show'}
     </div>
   )
 }
